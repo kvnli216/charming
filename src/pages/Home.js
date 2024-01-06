029bed2edc0475a8b7accb8dfd8d25971ab4b5c8
Kev <kev@Kevs-Mac-mini.local>
2024-01-06 17:36:51 -0500

    Install routing

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.scss"
+
+const Home = props => {
+  return (
+    <div className='container'>Home</div>
+  )
+}
+
+Home.propTypes = {}
+
+export default Home
