e66b77cf09445408fdbb6292cb8e67adf8a4eac0
Kev <kev@Kevs-Mac-mini.local>
2024-01-06 23:24:00 -0500

    Install mui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.scss"
+import { Tab, Tabs } from '@mui/material'
 
 const Home = props => {
+  const [tab, setTab] = useState()
+
+  const handleChange = (event, newValue) => {
+    setTab(newValue);
+  };
+
   return (
     <div className='container'>
       <div className='header'>
         <h1>Sabrina</h1>
+        <Tabs value={tab} onChange={handleChange}>
+          <Tab label='Reel' />
+          <Tab label='Work' />
+          <Tab label='Thesis' />
+          <Tab label='About' />
+        </Tabs>
       </div>
       <div className='content'>content</div>
       <div className='footer'>footer</div>
