a2443cb45ffae05b51651fc93740506080f9889e
Kev <kev@Kevs-Mac-mini.local>
2024-01-07 16:49:40 -0500

    Add css modules

--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import gif1 from '../../assets/003_test2.gif';
-import './index.css'
+import styles from './index.module.css'
 
 const Work = props => {
   return (
-    <div className='container'>
-      <img className='work-card' src={gif1} />
-      <img className='work-card' src={gif1} />
-      <img className='work-card' src={gif1} />
-      <img className='work-card' src={gif1} />
+    <div className={styles.container}>
+      <img className={styles['work-card']} src={gif1} />
+      <img className={styles['work-card']} src={gif1} />
+      <img className={styles['work-card']} src={gif1} />
+      <img className={styles['work-card']} src={gif1} />
     </div>
   )
 }
