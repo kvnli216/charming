8acc3169bd4bf3855ed41c3047ecc876250ec703
Kev <kev@Kevs-Mac-mini.local>
2024-01-09 13:02:53 -0500

    Implement hash routing with layout wrapper

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import {
   Routes,
   Route
 } from 'react-router-dom';
-import Home from './views/Home';
+import About from './views/About';
+import Layout from './views/Layout';
+import Work from './views/Work';
+import Illustration from './views/Illustration';
+import Reel from './views/Reel';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={'NoMatch'} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Work />} />
+          <Route path="/reel" element={<Reel />} />
+          <Route path="/illustration" element={<Illustration />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={'404 Page not found'} />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ src/views/Illustration/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Illustration = props => {
+  return (
+    <div>Illustration</div>
+  )
+}
+
+Illustration.propTypes = {}
+
+export default Illustration
