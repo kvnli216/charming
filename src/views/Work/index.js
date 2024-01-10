76ae6ec34d3fb52f28a73ae667d512d34a597867
Kev <kev@Kevs-Mac-mini.local>
2024-01-09 20:22:30 -0500

    Update Work View

--- src/views/Work/index.js
+++ src/views/Work/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import gif1 from '../../assets/003_test2.gif';
-import styles from './index.module.css'
+import gif1 from '../../assets/001_test.gif';
+import gif2 from '../../assets/002_test.gif';
+import gif3 from '../../assets/003_test2.gif';
+import styles from './index.module.css';
+
+const illustrations = [
+  gif1,
+  gif2,
+  gif3,
+];
 
 const Work = props => {
   return (
     <div className={styles.container}>
-      <img className={styles['work-card']} src={gif1} />
-      <img className={styles['work-card']} src={gif1} />
-      <img className={styles['work-card']} src={gif1} />
-      <img className={styles['work-card']} src={gif1} />
+      {illustrations.map((src) => {
+        console.log('src: ', src);
+        return <img className={styles['work-card']} src={src} alt='' loading='lazy' />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-Work.propTypes = {}
+Work.propTypes = {};
 
-export default Work
+export default Work;;
