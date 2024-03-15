d9d976e0c9ff24bbf2ac7fa0bed5fadaff61a4a7
Kev <kev@Kevs-Mac-mini.local>
2024-03-14 22:53:08 -0400

    Add 2024 Reel

--- src/views/Reel/index.js
+++ src/views/Reel/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-// import PropTypes from 'prop-types'
+import styles from './index.module.css';
 
 const Reel = props => {
   return (
-    <div>Reel</div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h2>2024 Reel</h2>
+      </div>
+      <div className={styles.content}>
+        <div className={styles['embed-wrapper']}>
+          <iframe
+            title={'Reel'}
+            src='https://player.vimeo.com/video/923468017?h=e458dba20c'
+            className={styles['iframe-video']}
+            frameborder="0"
+            allow="autoplay; fullscreen"
+            allowfullscreen
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
