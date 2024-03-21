6e357908ca56009fa73641cd17e813e88765e612
Kev <kev@Kevs-Mac-mini.local>
2024-03-21 15:27:28 -0400

    Update reel

--- src/views/Reel/index.js
+++ src/views/Reel/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from './index.module.css';
 import PropTypes from 'prop-types';
 
+const reelUrl = 'https://player.vimeo.com/video/925988365?h=a427296593';
+const title = 'MOTION REEL 2024';
+
 const Reel = ({ isMobile }) => {
   return (
     <>
@@ -10,13 +13,13 @@ const Reel = ({ isMobile }) => {
           ? (
             <div className={styles.container} >
               <div className={styles.header}>
-                <h3>2024 Reel</h3>
+                <h3>{title}</h3>
               </div>
               <div className={`${styles['content']} ${isMobile && styles['mobile']}`}>
                 <iframe
                   className={`${styles['iframe-video']} ${isMobile && styles['mobile']}`}
-                  title={'Reel'}
-                  src='https://player.vimeo.com/video/923468017?h=e458dba20c'
+                  title={title}
+                  src={reelUrl}
                   frameborder="0"
                   allow="autoplay; fullscreen"
                   allowfullscreen
@@ -27,13 +30,13 @@ const Reel = ({ isMobile }) => {
           : (
             <div className={styles.container} >
               <div className={styles.header}>
-                <h2>2024 Reel</h2>
+                <h2>{title}</h2>
               </div>
               <div className={styles.content}>
                 <iframe
                   className={styles['iframe-video']}
-                  title={'Reel'}
-                  src='https://player.vimeo.com/video/923468017?h=e458dba20c'
+                  title={title}
+                  src={reelUrl}
                   frameborder="0"
                   allow="autoplay; fullscreen"
                   allowfullscreen
