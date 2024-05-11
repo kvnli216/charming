b32418148435669ea6df87b5b02aacda6ae6788c
Kev <kev@Kevs-Mac-mini.local>
2024-05-11 11:39:25 -0400

    Update mobile layout

--- src/views/Detail/index.js
+++ src/views/Detail/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import styles from './index.module.css';
-import Player from '@vimeo/player';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./index.module.css";
+import Player from "@vimeo/player";
 
 const Detail = ({
   label,
@@ -11,7 +11,7 @@ const Detail = ({
   enableDetails = false,
 }) => {
   const handleTimeStamp = async (time) => {
-    const player = new Player(document.getElementById('media-iframe'));
+    const player = new Player(document.getElementById("media-iframe"));
     await player.setCurrentTime(time);
     await player.play();
   };
@@ -22,63 +22,89 @@ const Detail = ({
         <h1>{label}</h1>
       </div>
       <div className={styles.content}>
-        <div className={styles['embed-wrapper']}>
+        <div className={styles["embed-wrapper"]}>
           <iframe
-            id='media-iframe'
+            id="media-iframe"
             title={label}
             src={mediaUrl}
-            className={styles['iframe-video']}
+            className={styles["iframe-video"]}
             frameborder="0"
-            allow="autoplay; fullscreen; picture-in-picture" 
+            allow="autoplay; fullscreen; picture-in-picture"
             allowfullscreen
           />
         </div>
-        
-        {
-          enableDetails && (
-            <>
-            <div className={styles['subtitle-wrapper']}>
-          <p>
-            <span className={styles['link']} onClick={() => handleTimeStamp(0)}>00:00 - 00:04</span> Harper Chung in all aspects <br />
-            <span className={styles['link']} onClick={() => handleTimeStamp(4)}>00:04 - 00:26</span> Sabrina Chen in all aspects <br />
-            <span className={styles['link']} onClick={() => handleTimeStamp(26)}>00:26 - 00:51</span> Harper Chung in all aspects <br />
-            <span className={styles['link']} onClick={() => handleTimeStamp(52)}>00:52 - 01:04</span> Sabrina Chen in all aspects
-          </p>
 
-        </div>
-        <div className={styles['description-wrapper']}>
-          <h2>Description</h2>
-          <p>
-            "Stepping through the Seasons" is my thesis project at SVA Computer Arts.
-            It is a two person team project that combines 2D and 3D motion design.
-            We created a shoe apparel line called ZAS. This film promotes ZAS sports apparel through a commercial. It showcases four season-specific shoes, each featured in distinct settings and sports events.  The season segments end with a close-up of the product and a dynamic color and texture change to highlight differences in each shoe’s design.
-          </p>
-        </div>
-        <div className={styles['description-wrapper']}>
-          <h2>Why ZAS?</h2>
-          <p>
-            The voice of ZAS is dynamic, playful, and energetic. It is easy to remember and catchy.
-            It represents the speed and smoothness that athletes feel when they do sports.
-            We designed the bold, symmetric logo to make it good at showing the agility and fluidity that are important for sports.
-          </p>
-        </div>
-        <div className={styles['media-wrapper']}>
-          <img className={styles['image']} src={logo} alt='zas-logos' />
-        </div>
-        <div className={styles['description-wrapper']}>
-          <h2>Styleframes</h2>
+        {enableDetails && (
+          <>
+            <div className={styles["subtitle-wrapper"]}>
+              <p>
+                <span
+                  className={styles["link"]}
+                  onClick={() => handleTimeStamp(0)}
+                >
+                  00:00 - 00:04
+                </span>{" "}
+                Harper Chung in all aspects <br />
+                <span
+                  className={styles["link"]}
+                  onClick={() => handleTimeStamp(4)}
+                >
+                  00:04 - 00:26
+                </span>{" "}
+                Sabrina Chen in all aspects <br />
+                <span
+                  className={styles["link"]}
+                  onClick={() => handleTimeStamp(26)}
+                >
+                  00:26 - 00:51
+                </span>{" "}
+                Harper Chung in all aspects <br />
+                <span
+                  className={styles["link"]}
+                  onClick={() => handleTimeStamp(52)}
+                >
+                  00:52 - 01:04
+                </span>{" "}
+                Sabrina Chen in all aspects
+              </p>
+            </div>
+            <div className={styles["description-wrapper"]}>
+              <h2>Description</h2>
+              <p>
+                "Stepping through the Seasons" is my thesis project at SVA
+                Computer Arts. It is a two person team project that combines 2D
+                and 3D motion design. We created a shoe apparel line called ZAS.
+                This film promotes ZAS sports apparel through a commercial. It
+                showcases four season-specific shoes, each featured in distinct
+                settings and sports events. The season segments end with a
+                close-up of the product and a dynamic color and texture change
+                to highlight differences in each shoe’s design.
+              </p>
+            </div>
+            <div className={styles["description-wrapper"]}>
+              <h2>Why ZAS?</h2>
+              <p>
+                The voice of ZAS is dynamic, playful, and energetic. It is easy
+                to remember and catchy. It represents the speed and smoothness
+                that athletes feel when they do sports. We designed the bold,
+                symmetric logo to make it good at showing the agility and
+                fluidity that are important for sports.
+              </p>
+            </div>
+            <div className={styles["media-wrapper"]}>
+              <img className={styles["image"]} src={logo} alt="zas-logos" />
+            </div>
+            <div className={styles["description-wrapper"]}>
+              <h2>Styleframes</h2>
+            </div>
 
-          <div className={styles['styleframe-wrapper']}>
-            {styleframes.map((src) => (
-              <img src={src} alt='' />
-            ))}
-          </div>
-        </div>
-        </>
-          )
-
-        }
-       
+            <div className={styles["styleframe-wrapper"]}>
+              {styleframes.map((src) => (
+                <img src={src} alt="" />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
@@ -89,4 +115,4 @@ Detail.propTypes = {
   mediaUrl: PropTypes.string,
 };
 
-export default Detail;;
+export default Detail;
