b2823981985d2707b4900c451c8f09ce2b630ab9
Kev <kev@Kevs-Mac-mini.local>
2026-02-08 22:33:53 -0500

    Updates to layout and fonts

--- src/components/ThemeProvider/index.js
+++ src/components/ThemeProvider/index.js
@@ -5,12 +5,28 @@ export const theme = createTheme({
     MuiTab: {
       styleOverrides: {
         root: {
-          fontFamily: 'Garet-Book',
-          fontWeight: 'bold',
+          fontFamily: 'SF Light',
+          textTransform: 'capitalize',
           color: 'white',
           fontSize: '1.6rem',
+          "&.Mui-selected": {
+            color: "white",
+          },
         },
       },
     },
+    MuiTabs: {
+      styleOverrides: {
+        root: {
+          height: '100%',
+        },
+        scroller: {
+          display: 'flex',
+        },
+        indicator: {
+          backgroundColor: 'white',
+        }
+      },
+    }
   },
 });
