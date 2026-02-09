f22af0e21e3908f5a19a51bf5135ed2ed9004b00
Kev <kev@Kevs-Mac-mini.local>
2026-02-08 22:52:21 -0500

    Update Play page and copy

--- src/views/Illustration/index.js
+++ src/views/Illustration/index.js
@@ -1,52 +1,26 @@
 import React, { useEffect, useRef } from 'react';
-import jpg1 from '../../assets/Illustrations/001_test-small.jpg';
-import jpg2 from '../../assets/Illustrations/002_test-small.jpg';
-import jpg3 from '../../assets/Illustrations/003_test-small.jpg';
-import gif1 from '../../assets/Illustrations/001_test.gif';
-import gif2 from '../../assets/Illustrations/002_test.gif';
-import gif3 from '../../assets/Illustrations/003_test2.gif';
-import IMG_5860 from '../../assets/Illustrations/IMG_5860.jpg';
-import IMG_5861 from '../../assets/Illustrations/IMG_5861.jpg';
-import IMG_5863 from '../../assets/Illustrations/IMG_5863.jpg';
-import IMG_5864 from '../../assets/Illustrations/IMG_5864.jpg';
-import IMG_5865 from '../../assets/Illustrations/IMG_5865.jpg';
-import IMG_5867 from '../../assets/Illustrations/IMG_5867.jpg';
-import IMG_6090 from '../../assets/Illustrations/IMG_6090.jpg';
-import IMG_6091 from '../../assets/Illustrations/IMG_6091.jpg';
-import IMG_5860_small from '../../assets/Illustrations/IMG_5860-small.jpg';
-import IMG_5861_small from '../../assets/Illustrations/IMG_5861-small.jpg';
-import IMG_5863_small from '../../assets/Illustrations/IMG_5863-small.jpg';
-import IMG_5864_small from '../../assets/Illustrations/IMG_5864-small.jpg';
-import IMG_5865_small from '../../assets/Illustrations/IMG_5865-small.jpg';
-import IMG_5867_small from '../../assets/Illustrations/IMG_5867-small.jpg';
-import IMG_6090_small from '../../assets/Illustrations/IMG_6090-small.jpg';
-import IMG_6091_small from '../../assets/Illustrations/IMG_6091-small.jpg';
-import IMG_6093 from '../../assets/Illustrations/IMG_6093.jpg';
-import IMG_6093_small from '../../assets/Illustrations/IMG_6093-small.jpg';
-import IMG_6305 from '../../assets/Illustrations/IMG_6305.jpg';
-import IMG_6305_small from '../../assets/Illustrations/IMG_6305-small.jpg';
-import IMG_6306 from '../../assets/Illustrations/IMG_6306.jpg';
-import IMG_6306_small from '../../assets/Illustrations/IMG_6306-small.jpg';
-import 角色設計_頭髮 from '../../assets/Illustrations/角色設計_頭髮.jpg';
-import 角色設計small from '../../assets/Illustrations/角色設計_頭髮.jpg';
 import PropTypes from 'prop-types';
+import Scene24 from '../../assets/Illustrations/Scene24-square.gif';
+import Scene4 from '../../assets/Illustrations/Scene4-square.gif';
+import Scene28 from '../../assets/Illustrations/Scene28-square.gif';
+import Scene7 from '../../assets/Illustrations/Scene7-square.gif';
+import Scene10 from '../../assets/Illustrations/Scene10-square.gif';
+import Scene13 from '../../assets/Illustrations/Scene13-square.gif';
+import Scene21 from '../../assets/Illustrations/Scene21-square.gif';
+import Scene1 from '../../assets/Illustrations/Scene1-square.gif';
+import Scene9 from '../../assets/Illustrations/Scene9-square.gif';
 import styles from './index.module.css';
 
 const illustrations = [
-  { src: gif1, skeleton: jpg1 },
-  { src: gif2, skeleton: jpg2 },
-  { src: gif3, skeleton: jpg3 },
-  { src: IMG_6306, skeleton: IMG_6306_small },
-  { src: IMG_5860, skeleton: IMG_5860_small },
-  { src: IMG_5861, skeleton: IMG_5861_small },
-  { src: IMG_5863, skeleton: IMG_5863_small },
-  { src: IMG_5864, skeleton: IMG_5864_small },
-  { src: 角色設計_頭髮, skeleton: 角色設計small },
-  { src: IMG_5865, skeleton: IMG_5865_small },
-  { src: IMG_5867, skeleton: IMG_5867_small },
-  { src: IMG_6090, skeleton: IMG_6090_small },
-  { src: IMG_6091, skeleton: IMG_6091_small },
-  { src: IMG_6093, skeleton: IMG_6093_small },
+  { src: Scene24, skeleton: '' },
+  { src: Scene4, skeleton: '' },
+  { src: Scene28, skeleton: '' },
+  { src: Scene7, skeleton: '' },
+  { src: Scene10, skeleton: '' },
+  { src: Scene13, skeleton: '' },
+  { src: Scene21, skeleton: '' },
+  { src: Scene1, skeleton: '' },
+  { src: Scene9, skeleton: '' },
 ];
 
 const Card = ({
